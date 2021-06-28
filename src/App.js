5661bdf1e66174d056b1b81325a1172d6501180c
samueldusek <smldsk@protonmail.com>
2021-06-28 19:46:12 +0200

    add functions to generate different shadows for all colors in palette

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Palette from "./Palette";
 import seedColors from "./SeedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div className="App">
       <Palette {...seedColors[1]} />
