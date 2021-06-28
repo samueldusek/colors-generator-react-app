d672d0100c4f98ebd4f7ef38b8279da0613159e7
samueldusek <smldsk@protonmail.com>
2021-06-28 20:14:48 +0200

    add Navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "rc-slider/assets/index.css";
+import "./Navbar.css";
+
+import Slider from "rc-slider";
+
+class Navbar extends Component {
+  render() {
+    const { level, changeLevel } = this.props;
+    return (
+      <header className="Navbar">
+        <div className="logo">
+          <a href="/">reactcolorpicker</a>
+        </div>
+        <div className="slider-container">
+          <span>Lavel: {level}</span>
+          <div className="slider">
+            <Slider
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}
+            />
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Navbar;
--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
 import "./Palette.css";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
 
 class Palette extends Component {
   constructor(props) {
@@ -23,13 +22,7 @@ class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
