3255e08216bf2ffed3cbef2024a5a558d043e04b
samueldusek <smldsk@protonmail.com>
2021-06-28 19:56:20 +0200

    add slider to change colors shades

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   console.log(generatePalette(seedColors[4]));
   return (
     <div className="App">
-      <Palette {...seedColors[1]} />
+      <Palette palette={generatePalette(seedColors[3])} />
     </div>
   );
 }
